dc69b45dfc39f0df5e2eb18377ab6560f0b6ccfb
wilson millan <skullwalkerwv59@gmail.com>
2017-01-13 21:26:47 -0500

    proyecto con prueba de interfaz

---
+++ Assets/scripts/play.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class play : MonoBehaviour {
+
+	public void playGame (){
+		
+	}
+
+	public void ShowCredits(){
+		
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
