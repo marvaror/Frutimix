77bded9db4c189df1a66b499ab354f5dc6f178df
wilson millan <skullwalkerwv59@gmail.com>
2017-01-19 16:27:21 -0500

    -hice algunos cambios en nombres de prefabs y mejorando la parte visual del canvas

--- Assets/scripts/objectdrag.cs
+++ Assets/scripts/objectdrag.cs
@@ -7,10 +7,19 @@ using UnityEngine.EventSystems;
 // Analysis disable CheckNamespace
 public class objectdrag : MonoBehaviour ,IDragHandler,IBeginDragHandler,IEndDragHandler {
 // Analysis restore CheckNamespace
-	
+	//public GameObject []baterias;
+
+	/*public void OnClick(){
+		GameObject clon = Instantiate (baterias [0], transform.position, transform .rotation)as GameObject;
+	}*/
+
 	//detectamos el primer click para mover objeto
 	public void OnBeginDrag (PointerEventData eventdata){
 		Debug.Log ("onbegindrag");
+
+		this.transform.SetParent(this.transform.parent);
+		//GameObject clon = Instantiate (baterias [0], transform.position, transform
+		//	.rotation)as GameObject;
 		
 	}
 
