e0a6e521070b9881ef7e64cd8ea41bf78bbf9cb4
marvaror <marvaror@gmail.com>
2017-01-30 21:14:54 -0500

    Comunicacion entre botones de menu y creditos

---
+++ Assets/scripts/View/Credits.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour {
+    public GameObject menu;
+    public GameObject credi;
+	// Use this for initialization
+	public void Menu()
+    {
+        menu.SetActive(true);
+        credi.SetActive(false);
+    }
+
+    public void Creditos()
+    {
+        menu.SetActive(false);
+        credi.SetActive(true);
+    }
+}
